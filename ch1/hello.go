39554605c1e40fc04147349dd45734a0ffe55460
juanesgl <juan.sgarcia@mail.escuelaing.edu.co>
2026-09-18 09:11:47 -0500

    my firsttt hello worldd on Go

---
+++ ch1/hello.go
@@ -0,0 +1,7 @@
+package main // Declares the main package. A Go program starts from the main function in this package.
+
+import "fmt" // Imports the "fmt" package from the standard library. Go imports entire packages, not individual functions.
+
+func main() {
+	fmt.Println("Hello World")
+}
